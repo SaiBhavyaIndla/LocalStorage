55ddc75b8ed7546b2621eb7fdedbe25670e05911
SaiBhavyaIndla <indlasaibhavya>
2023-01-11 16:18:20 +0530

    task5

--- dom.js
+++ dom.js
@@ -29,14 +29,26 @@
 //headerTitle
 
 // GET ELEMENT BY CLASS NAME
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-//items[1].textContent='hello';
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// //items[1].textContent='hello';
 
-items[2].style.backgroundColor='green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
+// items[2].style.backgroundColor='green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+
+
+// GET ELEMENT BY TAG NAME
+var items=document.getElementsByTagName('list-group-item');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='hello';
+
+li[2].style.backgroundColor='green';
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
 }
 
 
