1e7e75ec6b8bba6dfc762ff378e3985cf9384571
SaiBhavyaIndla <indlasaibhavya>
2023-01-11 14:44:13 +0530

    Task3

---
+++ dom.js
@@ -0,0 +1,43 @@
+// EXAMINE THE DOCUMENT OBJECT//
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title= 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent='Hello';
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title').textContent = "Store Items";
+var headerTitle=document.getElementById('header-title').style.borderBottom='solid 3px #000';
+var main=document.getElementById('main').style.color='green';
+var main=document.getElementById('main').style.fontWeight='bold';
+//console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='GoodBye';
+// console.log(headerTitle.textContent);
+
+//headerTitle.innerHTML='<h3>Hello</h3>';
+//headerTitle
+
+// GET ELEMENT BY CLASS NAME
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='hello';
+// items.style.fontWeight='bold';
+// item[1].style.backgroundColor='yellow';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor='yellow';
+// }
+
+
+
