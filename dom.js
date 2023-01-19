279da267331eb90fde46b506d26b06c872eb567a
SaiBhavyaIndla <indlasaibhavya>
2023-01-19 12:25:27 +0530

    task 8

--- dom.js
+++ dom.js
@@ -150,11 +150,57 @@
 // var head = document.getElementById("main-header");
 // head.insertBefore(addHead,head);
 
-var addItem = document.createElement('li');
-var addText = document.createTextNode('Hello');
-addItem.appendChild(addText);
-var list = document.getElementById("items");
-list.insertBefore(addItem,list.children[0]);
+// var addItem = document.createElement('li');
+// var addText = document.createTextNode('Hello');
+// addItem.appendChild(addText);
+// var list = document.getElementById("items");
+// list.insertBefore(addItem,list.children[0]);
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+// form submit event
+form.addEventListener('submit' , addItem);
+// Delete event
+itemList.addEventListener('click' , removeItem);
+// Add item
+function addItem(e){
+    e.preventDefault();
+
+    // Get input value
+    var newItem = document.getElementById('item').value;
+
+    //Create new li element
+    var li = document.createElement('li');
+    //Add class
+    li.className = 'list-group-item';
+    //console.log(li);
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+    // Create del button element
+    var deleteBtn = document.createElement('button');
+
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // Append text Node
+    deleteBtn.appendChild(document.createTextNode('X'));
+    // Append button to li
+    li.appendChild(deleteBtn);
+    //Append li to list
+    itemList.appendChild(li);
+
+    
+}
+
+// Remove Item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
 
 
 
